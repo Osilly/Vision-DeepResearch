f923c03afe5deccd8aa27b11e6b56feb1cc4dbbe
Yu Zeng <18803425504@mail.ustc.edu.cn>
2026-02-06 14:50:41 +0800

    Update RL data convert scripts

---
+++ rllm/vision_deepresearch_async_workflow/data_prepare/convert_parquet2jsonl.sh
@@ -0,0 +1,4 @@
+python3 convert_parquet2jsonl.py \
+    --parquet_path Vision-DeepResearch-Toy-RL-Data/vision-deepresearch_RL_Demo_1k.parquet \
+    --output_jsonl Vision-DeepResearch-Toy-RL-Data/vision-deepresearch_RL_Demo_1k.jsonl \
+    --image_dir Vision-DeepResearch-Toy-RL-Data//image
