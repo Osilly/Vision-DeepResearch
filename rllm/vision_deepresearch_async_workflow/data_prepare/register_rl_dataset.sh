44dde0658db053a469a0e8786acb963a55757d2f
Yu Zeng <18803425504@mail.ustc.edu.cn>
2026-02-06 15:07:28 +0800

    Update RL data register scripts

---
+++ rllm/vision_deepresearch_async_workflow/data_prepare/register_rl_dataset.sh
@@ -0,0 +1,5 @@
+python3 register_dataset.py \
+    --jsonl_path Vision-DeepResearch-Toy-RL-Data/vision-deepresearch_RL_Demo_1k.jsonl \
+    --register_name Vision-DeepResearch-QA \
+    --train_ratio 0.9 \
+    --random_seed 42
